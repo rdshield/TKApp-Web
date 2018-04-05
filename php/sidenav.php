b5c50520c7820758e89b9e5d329feebe74700340
rdshield <rdshield@uci.edu>
2018-04-05 15:56:13 -0700

    Sidebar Setup
    Used PHP to change sidebar based on page (Login Page vs Pages that will be "Signed in" only)

--- php/sidenav.php
+++ php/sidenav.php
@@ -1,8 +1,22 @@
-<div class="sidenav" id="sidebar" style="display:block">
-	<p> Development Links </p>
-	<a href="/index.php"> Home Page (Index) </a>
-	<a href="php/register.php"> Register </a>
-	<a href="/php/children.php"> Children </a>
-	<a href="/php/admin.php"> Admin </a>
-	<a href="/php/report.php"> Report (in Admin) </a>
-</div>
+<?php
+function curPageName() {
+	return substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+}
+
+if (strpos(curPageName(),"login") !== false) { ?>
+
+<?php
+} else {
+?>
+	<div class="sidenav" id="sidebar" style="display:none">
+		<p> Development Links </p>
+		<a href="/php/register.php"> Register </a>
+		<a href="/php/login.php"> Login </a>
+		<a href="/php/children.php"> Children </a>
+		<a href="/php/admin.php"> Admin </a>
+		<a href="/php/report.php"> Report </a>
+	</div>echo "NO!";
+<?php
+}
+?>
+
